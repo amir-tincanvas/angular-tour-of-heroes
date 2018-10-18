ed3501e3ca18f745939cb3f8e888f66467b44b0c
Amir Hajizadeh <amir@tincanvas.com>
2018-10-18 01:59:35 -0700

    completed tutorial; converted services to make http calls to test server/db

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,19 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  getHeroes():void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
   }
-  
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
   constructor(private heroService: HeroService) { 
@@ -28,4 +30,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  getHeroes():void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
